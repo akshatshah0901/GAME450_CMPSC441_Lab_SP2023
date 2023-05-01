4e168f1f1ecd7332bc9fabbd1fe7bfd5323033f9
akshatshah0930 <akshat.shah0901@gmail.com>
2023-05-01 17:46:29 -0400

    Lab 3 Changes

--- src/lab3/travel_cost.py
+++ src/lab3/travel_cost.py
@@ -8,6 +8,10 @@ In this lab, you will write a function that calculates the cost of a route betwe
 A terrain is generated for you 
 '''
 import numpy as np
+import numpy as np
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid 
+from pathfinding.finder.a_star import AStarFinder
 
 def get_route_cost(route_coordinate, game_map):
     """
@@ -39,7 +43,10 @@ def get_route_cost(route_coordinate, game_map):
     :return: a floating point number representing the cost of the route
     """
     # Build a path from start to end that looks like [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 4)]
-    grid = Grid(width = width, height = width
+    width = max(game_map.shape[0], game_map.shape[1])
+    grid = Grid(width = width, height = width)
+
+    start = grid.node(route_coordinate[0][0], route_coordinate[0][1])
     matrix = max(game_map.shape[0],game_map.shape[1])
     finder = AStarFinder(diagonal_movement = DiagonalMovement.always)
     start = grid.node(route_coordinate[0][0], route_coordinate[0][1])
